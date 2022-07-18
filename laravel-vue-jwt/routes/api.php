720fb044eeaa40b4434158febb41e1fc2a993f85
sodaler <misha_lector87@mail.ru>
2022-07-19 00:35:36 +0400

    jwt tymon auth; Login, refresh, expired tokens

--- laravel-vue-jwt/routes/api.php
+++ laravel-vue-jwt/routes/api.php
@@ -18,13 +18,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+    Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
+        Route::get('/', 'IndexController');
+    });
+});
 
 Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
     Route::post('/', 'StoreController');
 });
-
-Route::group(['namespace' => 'Fruit', 'prefix' => 'fruits'], function () {
-    Route::get('/', 'IndexController');
-});
-
-
